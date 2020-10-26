c91e1983f6c033f28f3fb8c0ef5917701292dc23
scurry222 <scurry222@gmail.com>
2020-10-26 12:35:21 -0700

    remove error by including three source code, adding script in html

--- scripts/index_1.js
+++ scripts/index_1.js
@@ -1,4 +1,5 @@
-import * as THREE from 'three';
+import * as THREE from '../js/three.js';
+// const THREE = require('three');
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
