fb714040e942581aa95b51f3312fdf0cba468f16
scurry222 <scurry222@gmail.com>
2020-10-27 18:03:12 -0700

    separate into js file

--- scripts/index_1.js
+++ scripts/index_1.js
@@ -1,5 +1,4 @@
-import * as THREE from '../js/three.js';
-// const THREE = require('three');
+import '../js/three.js';
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -8,16 +7,20 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize ( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( {color : 0x00ff00} );
-var cube = new THREE.Mesh( geometry, material);
-scene.add( cube );
+var geometry_a = new THREE.IcosahedronGeometry();
+var material_a = new THREE.MeshBasicMaterial( {color : 0x00ff00, wireframe: true} );
+var cube_a = new THREE.Mesh( geometry_a, material_a );
+scene.add( cube_a );
+
+var geometry_b = new THREE.BoxGeometry
 
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame( animate );
-    renderer.Render( scene, camera );
+    cube_a.rotation.x += .01;
+    cube_a.rotation.y += .01;
+    renderer.render( scene, camera );
 }
 
-animate()
+animate();
