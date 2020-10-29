e5ec69001aba445c39e70cf83a5dff51fe33ed3f
Jack Gindi <jmgindi@gmail.com>
2020-10-29 13:14:25 -0700

    attempting to project BufferGeometry

--- scripts/index_1.js
+++ scripts/index_1.js
@@ -1,25 +1,27 @@
 import * as THREE from 'three';
+import Board from './board';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize ( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry_a = new THREE.IcosahedronGeometry();
-var material_a = new THREE.MeshBasicMaterial( {color : 0x00ff00, wireframe: true} );
-var cube_a = new THREE.Mesh( geometry_a, material_a );
-scene.add( cube_a );
+const geometry_a = new THREE.IcosahedronGeometry();
+const material_a = new THREE.MeshBasicMaterial( {color : 0x00ff00, wireframe: true} );
+const cube_a = new THREE.Mesh( geometry_a, material_a );
+//scene.add( cube_a );
 
-var geometry_b = new THREE.BoxGeometry
+const board = new Board.createBoard();``
+scene.add(board);
 
+//camera position
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame( animate );
-    cube_a.rotation.x += .01;
-    cube_a.rotation.y += .01;
+    
     renderer.render( scene, camera );
 }
 
