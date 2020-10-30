7f4351aaa73caccdc3711d86cb758154e77aee7f
Jack Gindi <jmgindi@gmail.com>
2020-10-30 15:34:17 -0700

    added more functionality to boardController including geometry for board and ground

--- scripts/boardController.js
+++ scripts/boardController.js
@@ -13,9 +13,17 @@ export default class BoardController {
         this.scene;
         this.camera;
         this.cameraController;
+        this.lights = {};
+        this.materials = {};
+        this.pieceGeometry = null;
+        this.boardModel;
+        this.groundModel;
+        this.squareSize = 10;
 
         this.drawBoard = function() {
             this.initEngine();
+            this.initLights();
+            this.initMaterials();
             this.initObjects(function () {
                 this.onAnimationFrame();
             }.bind(this));
@@ -45,11 +53,68 @@ export default class BoardController {
         this.containerE1.appendChild(this.renderer.domElement);
     }
     
+    initLights = () => {
+        this.lights.topLight = new THREE.PointLight();
+        this.lights.topLight.position.set(0, 150, 0);
+        this.lights.topLight.intensity = 1.0;
+
+        this.scene.add(this.lights.topLight);
+    }
+
+    initMaterials = () => {
+        this.materials.boardMaterial = new THREE.MeshLambertMaterial({
+            color: 0xdd2200
+        });
+        this.materials.groundMaterial = new THREE.MeshBasicMaterial({
+            transparent: true,
+            color: 0x9999ff
+        })
+        this.materials.darkSquareMaterial = new THREE.MeshLambertMaterial({
+            color: 0x5c1400
+        });
+        this.materials.lightSquareMaterial = new THREE.MeshLambertMaterial({
+            color: 0xe6e4da
+        });
+        this.materials.whitePieceMaterial = new THREE.MeshPhongMaterial({
+            color: 0xe9e4bd,
+            shininess: 20
+        });
+        this.materials.blackPieceMaterial = new THREE.MeshPhongMaterial({
+            color: 0x9f2200,
+            shininess: 20
+        });
+        this.materials.pieceShadowPlane = new THREE.MeshBasicMaterial({
+            transparent: true,
+            color: 0x111111
+        });
+    }
+
+    initBoard = () => {
+        const boardGeometry = new THREE.BoxGeometry(200, 15, 200);
+        const board = new THREE.Mesh(boardGeometry, this.materials.boardMaterial);
+        this.scene.add(board);
+    }
+
+    initGround = () => {
+        this.groundModel = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000, 1, 1, materials.groundMaterial));
+        this.groundModel.position.set(this.squareSize * 4, -1.52, this.squareSize * 4);
+        this.groundModel.rotation.x = -90 * Math.PI / 180;
+        this.scene.add(this.groundModel);
+    }
+
     initObjects = (callback) => {
-        const geometry = new THREE.BoxGeometry(50, 0, 50);
-        const material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
-        const cube = new THREE.Mesh(geometry, material);
-        this.scene.add(cube)
+
+        this.scene.add(new THREE.AxesHelper(200));
+
+        this.initBoard()
+
+        // for (let x = 0; x <= 31; x++) {
+        //     if (x > 15) {
+        //         initPiece(this.materials.whitePieceMaterial)
+        //     } else {
+        //         initPiece(this.materials.blackPieceMaterial)
+        //     }
+        // }
     
         callback();
     }
