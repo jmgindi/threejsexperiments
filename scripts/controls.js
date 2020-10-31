890be61fbc29ae4fac095a9b4f2196030330415f
scurry222 <scurry222@gmail.com>
2020-10-30 18:05:04 -0700

    add camera toggle button

--- scripts/controls.js
+++
@@ -1,7 +0,0 @@
-// export const cameraToggle = () => {
-//     document.addEventListener('click', (e) => {
-//         if (!e.target.matches('.camera-toggle')) return;
-//         this.cameraController.enabled = !this.cameraController.enabled;
-//     });
-// }
-
